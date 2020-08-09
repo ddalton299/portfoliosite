ee3e2fd30a0e208536420da002e046091e1c38bf
ddalton299 <denzeldalton2119>
2020-08-09 17:13:54 -0400

    Button Fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -16,8 +16,12 @@ class Landing extends Component {
             <h4>Technology Professional </h4>
             <h5>Email: ddalton2119@gmail.com</h5>
             <img id="Photo" src={MyPhoto}/>
-            <a className="btn btn-outline-primary" href="/AboutMe">About Me</a>
-            <a className="btn btn-outline-primary" href="/AboutSite">About This Site</a>    
+            <div>
+              <a className="btn btn-outline-primary btn-block" href="/AboutMe">About Me</a>
+              <a className="btn btn-outline-primary btn-block" href="/AboutSite">About This Site</a>
+            </div>
+            
+                
           </div>
         
         </div>
